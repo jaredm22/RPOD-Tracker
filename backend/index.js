077e0b83c3d7d5e4c63adf272c2f402487cdcf91
Jared Min <jkymin2@gmail.com>
2021-12-01 17:09:25 -0500

    Fix cors issue

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,12 @@ const app = express();
 
 const url = "mongodb+srv://jaredm22:jjluke77@cluster0.txzog.mongodb.net/mlbplayers?retryWrites=true&w=majority";
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 mongoose.connect(url, { useNewUrlParser: true });
 const db = mongoose.connection
